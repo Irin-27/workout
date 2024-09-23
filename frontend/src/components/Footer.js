71eb4cf2ffe5f4ebdc330064dd5d0284dbdf3908
Irin-27 <singh.nitin2714@gmail.com>
2024-09-23 12:41:02 +0530

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+return (
+  <footer>
+    <div className="footer">
+      
+        Copyright &copy; FitOrbit 2024 
+      
+    </div>
+  </footer>
+)
+}
+
+export default Footer
