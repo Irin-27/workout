af783b80102e6124f16e945a6e096677e6706f82
Irin-27 <singh.nitin2714@gmail.com>
2025-02-13 13:53:23 +0530

    commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 const axios=require('axios')
+const cron=require('node-cron')
 // express app
 const app = express()
 
