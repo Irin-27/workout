fbedf7d0023499ec50825f1bc51b4665af58b857
Irin-27 <singh.nitin2714@gmail.com>
2025-02-13 13:16:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+const axios=require('axios')
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+app.get('/ping', (req, res) => {
+  console.log(`Ping received at ${new Date().toISOString()}`);
+  res.json({ message: "Server is awake" });
+});
+
+// Cron Job
+cron.schedule('*/14 * * * *', async () => {
+  try {
+      console.log('Running cron job');
+      const response = await axios.get(`http://localhost:4000/ping`);
+      console.log('API response:', response.data);
+  } catch (error) {
+      console.error('Error in cron job:', error);
+  }
+});
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
